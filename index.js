3ec40d86b02ba2c30abe07d15214f86dd725210c
PH <15917977183@163.com>
2021-09-30 09:42:47 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello github actions with nodejs！！！')
+import axios from 'axios';
+const fetchURL = async function (token) {
+  const ax = axios.create({
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const res = await ax.get(
+    `https://api.github.com/repos/phsix/phsix.github.io/issues`
+  );
+};
+
+fetchURL(process.argv[2]);
